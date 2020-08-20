92811b0f919312e6f8d8d5f8bb6deb5dc01c2684
SmithAmbition <cranelvjq@163.com>
2020-08-20 15:36:41 +0800

    truffle config

--- migrations/2_tokenGeyser_migration.js
+++ migrations/2_tokenGeyser_migration.js
@@ -2,8 +2,7 @@ const Tokenliquidity = artifacts.require("TokenLiquidity");
 const FNXCoin = artifacts.require("FNXCoin");
 const UniCoin = artifacts.require("UniCoin");
 module.exports = async function(deployer, network,accounts) {
-    let fnx = await deployer.deploy(FNXCoin);
-    let uni = await deployer.deploy(UniCoin);
-    let expiration = Math.floor(Date.now()/1000) + 86400;
-    let liquidity = await deployer.deploy(Tokenliquidity,FNXCoin.address,UniCoin.address,86400,1e15,expiration);
+    let expiration = Math.floor(Date.now()/1000) + 86400*30;
+    let liquidity = await deployer.deploy(Tokenliquidity,"0xDd4b9b3Ce03faAbA4a3839c8B5023b7792be6e2C","0x69120197b77b51d32fFA5eAfe16b3d78115640c6",
+    86400*30,1e15,expiration);
 };
--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,21 @@
 
+function keystoreProvider (providerURL) {
+  const fs = require('fs');
+  const EthereumjsWallet = require('ethereumjs-wallet');
+  const HDWalletProvider = require('truffle-hdwallet-provider');
+
+  const KEYFILE = process.env.KEYFILE;
+  const PASSPHRASE = (process.env.PASSPHRASE || '');
+  if (!KEYFILE) {
+    throw new Error('Expected environment variable KEYFILE with path to ethereum wallet keyfile');
+  }
+
+  const KEYSTORE = JSON.parse(fs.readFileSync(KEYFILE));
+  const wallet = EthereumjsWallet.fromV3(KEYSTORE, PASSPHRASE);
+  return new HDWalletProvider(wallet._privKey.toString('hex'), providerURL);
+}
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const rinkeby = 'https://rinkeby.infura.io/v3/2521699167dc43c8b4c15f07860c208a';
 module.exports = {
   networks: {
     development: {
@@ -6,6 +23,15 @@ module.exports = {
       port: 7545,            // Standard Ethereum port (default: none)
       network_id: "*",       // Any network (default: none)
 //      gas: 7500000,
+    },
+    rinkeby : {
+          provider: () => new HDWalletProvider("",
+          rinkeby),
+          network_id: 4,   // This network is yours, in the cloud.
+  //       production: true    // Treats this network as if it was a public net. (default: false)
+          timeoutBlocks: 300,  // # of blocks before a deployment times out  (minimum/default: 50)
+          gasPrice: 1000000000,
+        skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
     }
   },
   mocha: {
